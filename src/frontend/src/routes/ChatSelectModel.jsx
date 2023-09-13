4782901808aa2f575d30ce06cd821ae216f5a66f
icpp <icpp@icpp.world>
2023-09-13 07:08:48 -0400

    Model Size into separate component

--- src/frontend/src/routes/ChatSelectModel.jsx
+++ src/frontend/src/routes/ChatSelectModel.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 import 'dracula-ui/styles/dracula-ui.css'
 import { Box, Button, Card, Heading, Divider, Text } from 'dracula-ui'
 
+import { ChatSelectModelSizeCardTiny } from './ChatSelectModelSizeCardTiny'
+
 export function ChatSelectModel({
   modelType,
   setModelType,
@@ -61,51 +63,7 @@ export function ChatSelectModel({
         </Button>
       </Card>
 
-      <Card
-        id="setModelSizeCard_TinyStories"
-        variant="subtle"
-        color="purple"
-        p="md"
-        m="md"
-      >
-        {/* <Box>
-          <Text color="white">Model Size: </Text>
-        </Box>
-
-        <Divider></Divider> */}
-        <Button
-          color={modelSize === '260K' ? 'white' : 'purple'}
-          size="lg"
-          p="2xl"
-          onClick={() => doSetModelSize('260K')}
-        >
-          260K
-        </Button>
-        <Button
-          color={modelSize === '15M' ? 'white' : 'purple'}
-          size="lg"
-          p="2xl"
-          onClick={() => doSetModelSize('15M')}
-        >
-          15M
-        </Button>
-        <Button
-          color={modelSize === '42M' ? 'white' : 'purple'}
-          size="lg"
-          p="2xl"
-          onClick={() => doSetModelSize('42M')}
-        >
-          42M
-        </Button>
-        <Button
-          color={modelSize === '110M' ? 'white' : 'purple'}
-          size="lg"
-          p="2xl"
-          onClick={() => doSetModelSize('110M')}
-        >
-          110M
-        </Button>
-      </Card>
+      <ChatSelectModelSizeCardTiny modelSize={modelSize} doSetModelSize={doSetModelSize} />
 
       <Card
         id="setPromptTypeCard"
