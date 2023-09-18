f028a26bc80d59fda84b8d557e0f51a6a3ee7466
icpp <icpp@icpp.world>
2023-09-18 12:30:40 -0400

    format

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -73,7 +73,7 @@ export function App() {
     inputPlaceholder,
     chatDisplay,
     heightChatInput,
-    isSubmitting
+    isSubmitting,
   ])
 
   // ---------------------------------------------------------
